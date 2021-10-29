c4c0e5350c00c4a2af0a6a75eb46e601b45a3d89
Iuri Ostrikov <iurii84@gmail.com>
2021-10-28 23:14:57 -0400

    change method name

--- test_items.py
+++ test_items.py
@@ -5,9 +5,8 @@ from selenium.webdriver.common.by import By
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_should_have_btn_to_add_items_to_basket(browser):
+def test_should_have_btn_to_add_items_to_cart(browser):
     browser.get(link)
-
     elements_found = browser.find_elements(By.CLASS_NAME, "btn-add-to-basket")
     count_elements = len(elements_found)
     assert count_elements == 1, "Qty of found elements is not equal to 1"
